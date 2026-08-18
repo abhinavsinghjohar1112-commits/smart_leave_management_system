617290f4bcea7ac0fb265f6f3a3044b6adaaaaeb
Abhinav <abhinavsinghjohar1112@gmail.com>
2026-08-18 15:18:17 +0530

    manageemployeechange2

--- app.py
+++ app.py
@@ -1043,7 +1043,7 @@ def manage_employees():
         phone,
         is_active,
         manager_id
-    FROM signupdata WHERE position = 'employee' or  position='manager'
+    FROM signupdata WHERE position = 'employee' OR  position='manager'
 """)
     employees = cursor.fetchall()
 
